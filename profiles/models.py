cf87510ea2a7c8d9b10bf4ffffca15442bef7137
chenchiyuan <chenchiyuan03@gmail.com>
2012-05-13 22:52:30 +0800

    can talk with user, tomorrow add user talk caches

--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from mongoengine.django.auth import User
-from mongoengine.fields import StringField, GeoPointField, FloatField, ListField
+from mongoengine.document import Document
+from mongoengine.fields import StringField, GeoPointField, FloatField, ListField, DateTimeField
 from core.const import SLUG_MAX, TEXT_MAX, PLACE_MSG_CENTER_PREFIX, FEEDS_PREFIX, DEFAULT_PASSWORD
 from places.models import Area
 from core.instant_search import SearchIndex
 from questions.models import Question
 from core.cache import cache
 import time
+import datetime
 
 __author__ = 'chenchiyuan'
 
@@ -18,10 +20,10 @@ class Profile(User):
         '''
 
     #static field
+    sns_id = StringField(max_length=TEXT_MAX, unique=True)
     username = StringField(max_length=SLUG_MAX, required=True, unique=True)
     password = StringField(max_length=TEXT_MAX)
     area_slug = StringField(max_length=SLUG_MAX, default='0 ')
-    sns_id = StringField(max_length=TEXT_MAX, default='')
     #logout clear it
     access_token = StringField(max_length=TEXT_MAX, default='')
     expires_in = FloatField(default=0.0)
@@ -35,7 +37,7 @@ class Profile(User):
     description = StringField(max_length=TEXT_MAX, default='')
 
     meta = {
-        'indexes': ['username']
+        'indexes': ['sns_id', 'username']
     }
 
     @property
@@ -141,3 +143,92 @@ class Profile(User):
         else:
             tags = tags
         self.update(set__tags=tags)
+
+    def talk_to(self, user, content=''):
+        session = Session.create_session(user_sns_id=user.sns_id, content=content)
+
+        talk = Talk.get_or_create_by_users(user_a=self.sns_id, user_b=user.sns_id)
+        if session:
+            talk.add_session(session)
+
+    def get_talk_history_with_user(self, user):
+        pass
+
+
+class Talk(Document):
+    #owner is two people who's sns_id smaller
+    owner = StringField(max_length=TEXT_MAX)
+    talk_to = StringField(max_length=TEXT_MAX, unique_with='owner')
+
+    history = ListField(StringField(max_length=TEXT_MAX), default=[])
+
+    meta = {
+        'indexes': [('owner', 'talk_to'),]
+    }
+
+    def add_session(self, session):
+        try:
+            self.update(push__history=str(session.id))
+        except Exception as err:
+            print(err)
+
+    @classmethod
+    def create_talk(cls, user_a, user_b):
+        owner, talk_to = cls.__get_owner_talk_to(user_a, user_b)
+
+        talk = Talk(owner=owner, talk_to=talk_to)
+        try:
+            talk.save()
+        except Exception as err:
+            print(err)
+            return None
+
+        return talk
+
+    @classmethod
+    def get_or_create_by_users(cls, user_a, user_b):
+        owner, talk_to = cls.__get_owner_talk_to(user_a, user_b)
+
+        try:
+            return cls.objects.get(owner=owner, talk_to=talk_to)
+        except Exception as err:
+            print(err)
+            return cls.create_talk(user_a, user_b)
+
+    @classmethod
+    def __get_owner_talk_to(cls, user_a, user_b):
+        tmp_user_a = float(user_a)
+        tmp_user_b = float(user_b)
+
+        owner = user_a if tmp_user_a < tmp_user_b else user_b
+        talk_to = user_a if tmp_user_a > tmp_user_b else user_b
+        return owner, talk_to
+
+
+
+class Session(Document):
+    user_sns_id = StringField(max_length=SLUG_MAX, required=True)
+    content = StringField(max_length=TEXT_MAX, default='')
+    updated_at = DateTimeField(default=datetime.datetime.now())
+
+    def to_info(self):
+        pass
+
+    @classmethod
+    def create_session(cls, user_sns_id, content, **kwargs):
+        session = Session(user_sns_id=user_sns_id, content=content)
+        try:
+            session.save()
+        except Exception as err:
+            print(err)
+            return None
+
+        return session
+
+    @classmethod
+    def get_by_id(cls, id):
+        try:
+            return cls.objects.get(id=id)
+        except Exception as err:
+            print(err)
+            return None
